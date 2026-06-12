a5a22cf6388a9b53e0c4f2ef439511da85b22589
Coraliaprog <crucetadelacruzcoralia@gmail.com>
2026-06-12 16:43:33 -0400

    Corrige entidad Comida en relación con Ingrediente (Issue #4)

--- SistemaGestionMenuSemanal/SistemaGestionMenuSemanal.API/Models/Entities/Comida.cs
+++ SistemaGestionMenuSemanal/SistemaGestionMenuSemanal.API/Models/Entities/Comida.cs
@@ -1,8 +1,13 @@
-﻿using System;
-
-public class Class1
+﻿namespace SistemaGestionMenuSemanal.API.Models.Entities
 {
-	public Class1()
-	{
-	}
+    public class Comida
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Tipo { get; set; } = string.Empty;
+        public string DiaSemana { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+
+        public List<Ingrediente> Ingredientes { get; set; } = new List<Ingrediente>();
+    }
 }
